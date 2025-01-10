0636fa34aed98a41ef3a356ef4e6fb5280982bfe
Shawn <38342848+shawnkan28@users.noreply.github.com>
2025-01-10 12:32:05 +0800

    export to csv

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import math
 import requests
 import pandas as pd
 from tqdm import tqdm
+from datetime import datetime
 
 DOC_ID = "nADf8mVx-6"
 URI = {
@@ -12,12 +13,24 @@ URI = {
 }
 HEADER = {'Authorization': 'Bearer {api_key}'}
 
+
 def main():
     set_api_key("./API_KEY.txt")
     # tns = get_all_table_names()  # Get all table names in doc
 
     # Get Table Data
     df = get_td("Historical Transactions")
+    # Process dtypes
+    df['Date'] = pd.to_datetime(df['Date'])
+    df['Value'] = df['Value'].str.replace(r"[\$,]", "", regex=True).astype(float)
+    df['Amount (SGD)'] = df['Amount (SGD)'].str.replace(r"[\$,]", "", regex=True).astype(float)
+
+    del df['Display']
+    del df['UUID']
+
+    print(df)
+    print(df.dtypes)
+    df.to_csv(f"Trans-{datetime.now().strftime('%Y%m%d-%H%M%S')}.csv", index=False)
 
 
 def get_all_table_names() -> list:
@@ -29,6 +42,7 @@ def get_all_table_names() -> list:
     assert res.status_code == 200
 
     return [x['name'] for x in res.json()['items'] if x['tableType'] != 'view']
+
 
 def get_td(tn: str) -> pd.DataFrame:
     """
@@ -53,8 +67,10 @@ def get_td(tn: str) -> pd.DataFrame:
         data = rsp.json()
         df_data += [x['values'] for x in data['items']]
         page_token = data.get('nextPageToken', None)
+    df = pd.DataFrame(df_data)
 
-    return pd.DataFrame(df_data)
+    return df
+
 
 def get_t_details(tn: str) -> dict:
     """
@@ -68,6 +84,7 @@ def get_t_details(tn: str) -> dict:
     data = rsp.json()
     return {"total": data['rowCount'], "id": data['id']}
 
+
 def set_api_key(path: str) -> None:
     """
 
@@ -76,6 +93,7 @@ def set_api_key(path: str) -> None:
     """
     with open(path, "r") as rf:
         HEADER['Authorization'] = HEADER['Authorization'].format(api_key=rf.read().strip())
+
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
