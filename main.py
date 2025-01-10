7ac6355bfc6a59f9179d42444555b20d4f03c8b2
Shawn <38342848+shawnkan28@users.noreply.github.com>
2025-01-10 15:07:26 +0800

    Update only the diff

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import math
-
+import glob
+import pathlib
+import re
 import requests
+import os
 import pandas as pd
 from tqdm import tqdm
 from datetime import datetime
@@ -27,12 +30,14 @@ def main():
     df['Amount (SGD)'] = df['Amount (SGD)'].str.replace(r"[\$,]", "", regex=True).astype(float)
 
     del df['Display']
-    del df['UUID']
 
     df.to_csv(f"Trans-{datetime.now().strftime('%Y%m%d-%H%M%S')}.csv", index=False)
 
     # Update Database using the extracted data
     update_db(df)
+
+    # Keep latest 7 only
+    keep_latest(r"Trans-.*\.csv")
 
 
 def update_db(df: pd.DataFrame):
@@ -43,11 +48,30 @@ def update_db(df: pd.DataFrame):
     """
     db = DB(host="localhost", port=27017, dbname="ExpenseTracker")
 
-    db.add_rows(df.to_dict("records"), "Transaction")
+    # get data in extracted df that is not in database df
+    df1 = db.show_table("Transaction")
+    if not df1.empty:
+        new_df = get_diff(df1, df)
+        print(f"Number of Rows to Update: {new_df.shape[0]}")
+        if not new_df.empty:
+            db.add_rows(new_df.to_dict("records"), "Transaction")
+    else:
+        db.add_rows(df.to_dict("records"), "Transaction")
 
-    df = db.show_table("Transaction")
-    print(df)
 
+def get_diff(df1: pd.DataFrame, df2: pd.DataFrame) -> pd.DataFrame:
+    """
+    Get Delta of df1 and df2. We want those in df2 that is not in df1.
+    :param df1: the main dataframe
+    :param df2: we want the values that are in df2  but not in df1
+    :return: dataframe
+    """
+    df = pd.merge(df1, df2, how="outer", on="UUID", indicator=True)
+    df = df.loc[df["_merge"] == "right"]
+
+    cols = list(df1.columns)
+    df.rename(columns={x + "_y": x for x in cols}, inplace=True)
+    return df[cols]
 
 def get_all_table_names() -> list:
     """
@@ -109,6 +133,35 @@ def set_api_key(path: str) -> None:
     """
     with open(path, "r") as rf:
         HEADER['Authorization'] = HEADER['Authorization'].format(api_key=rf.read().strip())
+
+
+def keep_latest(regex: str, num_keep=7, root_path="./") -> None:
+    """
+    Keep the last 7 output files
+    :param regex: regex for the files to keep track of
+    :param num_keep: number of files to keep
+    :param root_path: path to get the files from
+    :return:
+    """
+    files = sorted(get_filepaths(root_path, regex), reverse=True)
+    for x in files[num_keep:]:
+        os.remove(x)
+
+
+def get_filepaths(root_path: str, regex: str) -> list:
+    """
+    Get file paths that has the end file name using regex
+    :param root_path: path to search through
+    :param regex: the regex of files you want to extract
+    :return: list of file paths from regex
+    """
+    root_path = pathlib.Path(root_path)
+
+    for _, _, files in os.walk(root_path):
+        for file in files:
+            if re.match(regex, file):
+                yield (root_path / file).absolute()
+        break  # only iterate through the first layer
 
 
 # Press the green button in the gutter to run the script.
